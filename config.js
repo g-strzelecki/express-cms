e2a406cdea4c90e943757f8ccb704b78dea86096
g-strzelecki <grzesionmax@gmail.com>
2021-09-28 23:38:06 +0200

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:b2x2EiIiS8aI94uA@cluster0.ek3e3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const login = 'admin';
+const password = '123';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Logowanie' });
+});
+
+router.post('/login', function(req, res) {
+  const body = req.body;
+
+  if(body.login == login && body.password == password) {
+    req.session.admin = 1;
+    res.redirect('./admin');
+  } else {
+    res.redirect('./login');
+  }
 });
 
 module.exports = router;
