89e3b15988904452dd0c6c5caa5ebf942da225db
g-strzelecki <grzesionmax@gmail.com>
2021-09-29 21:07:13 +0200

    quiz form

--- routes/news.js
+++ routes/news.js
@@ -8,14 +8,11 @@ router.get('/', (req, res) => {
   const search = req.query.search || '';
 
   const findNews = News
-    .find({title: new RegExp(search.trim(), 'i')})
-    .sort({created: -1})
-    ;
-
+    .find({ title: new RegExp(search.trim(), 'i') })
+    .sort({ created: -1 });
   findNews.exec((err, data) => {
-
     res.render('news', { title: 'News', data, search });
-  })
+  });
 });
 
 module.exports = router;
